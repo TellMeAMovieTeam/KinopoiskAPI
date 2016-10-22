0372c88706c606b8f2ef6a4c786cbbcae162d20c
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-10-22 10:49:08 +0300

    Начал делать API

--- KinopoiskAPI/KinopoiskAPI/ViewController.swift
+++ KinopoiskAPI/KinopoiskAPI/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func getFilm(_ sender: AnyObject) {
+        
+    var test = ""
+        
+      KinopoiskAPI.getFilm(filmID: 714888) { result in
+            test = result
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
