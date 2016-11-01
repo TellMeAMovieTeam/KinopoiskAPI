a1beee8e4250b598ecb4574307dca1403ca4c811
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-11-01 23:29:02 +0300

    Сделал получение жанров в виде массива строк

--- KinopoiskAPI/KinopoiskAPI/Models/KinopoiskGenres.swift
+++ KinopoiskAPI/KinopoiskAPI/Models/KinopoiskGenres.swift
@@ -11,7 +11,7 @@ import Foundation
 open class KinopoiskGenres{
     
     static var genres : String = ""
-
+    
     public static func getGenres() {
         
         let getGenresURL = "https://api.kinopoisk.cf/getGenres"
@@ -19,7 +19,13 @@ open class KinopoiskGenres{
         HTTPRequest.request(urlString: getGenresURL) { result in
             genres = result
             
+            if let dataFromString = genres.data(using: .utf8, allowLossyConversion: false) {
+                
+                let json = JSON(data: dataFromString)
+                let arrayNames =  json["genreData"].arrayValue.map({$0["genreName"].stringValue})
+                print(arrayNames)
+            }
         }
     }
-
+    
 }
