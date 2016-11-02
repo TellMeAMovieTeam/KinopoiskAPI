287d90bd75cb01bae952316c56566777aa7e7d87
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-11-02 12:52:06 +0300

    Сделал класс для хранения данных о фильме

--- KinopoiskAPI/KinopoiskAPI/Models/KinopoiskFilm.swift
+++ KinopoiskAPI/KinopoiskAPI/Models/KinopoiskFilm.swift
@@ -16,8 +16,8 @@ open class KinopoiskFilm {
 
 public class Film {
     
-    var KinopoiskID : Int!
-    var IMDB_ID : Int!
+    var KinopoiskID : String!
+    var IMDB_ID : String!
     var WebURL : String = ""
     var NameRU : String = ""
     var NameEN : String = ""
@@ -31,7 +31,31 @@ public class Film {
     var RatingMPAA : String = ""
     var AgeLimits : Int = 0
     var RatingData : [FilmRating] = []
-
+    
+    init(FilmData : JSON) {
+        
+        self.KinopoiskID = FilmData["filmID"].stringValue
+        self.IMDB_ID = FilmData["imdbID"].stringValue
+        self.WebURL = FilmData["webURL"].stringValue
+        self.NameRU = FilmData["nameRU"].stringValue
+        self.NameEN = FilmData["nameEN"].stringValue
+        self.PosterURL = FilmData["posterURL"].stringValue
+        self.Year = FilmData["year"].intValue
+        self.FilmLength = FilmData["filmLength"].stringValue
+        self.Country = FilmData["country"].stringValue
+        self.Genre = FilmData["genre"].stringValue
+        self.Slogan = FilmData["slogan"].stringValue
+        self.Description = FilmData["description"].stringValue
+        self.RatingMPAA = FilmData["ratingMPAA"].stringValue
+        self.AgeLimits = FilmData["ratingAgeLimits"].intValue
+        
+        let MovieRatings = FilmData["ratingData"]
+        
+        self.RatingData.append(FilmRating(RatingName: "ratingKP", RatingValue: MovieRatings["rating"].floatValue as AnyObject))
+        self.RatingData.append(FilmRating(RatingName: "ratingKPVoteCount", RatingValue: MovieRatings["ratingVoteCount"].intValue as AnyObject))
+        self.RatingData.append(FilmRating(RatingName: "ratingIMDb", RatingValue: MovieRatings["ratingIMDb"].floatValue as AnyObject))
+        self.RatingData.append(FilmRating(RatingName: "ratingIMDbVoteCount", RatingValue: MovieRatings["ratingIMDbVoteCount"].intValue as AnyObject))
+    }
 }
 
 
